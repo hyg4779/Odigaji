12c77c528b32ff4fbef0604122dc61b2efd02ef1
junaem <junh9547@gmail.com>
2022-03-22 15:39:05 +0900

    refactoring:[back] 불필요한 import문 삭제

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 
