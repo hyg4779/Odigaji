83c26f26e092201b4c1f9ec44ac75d7c00fa89a6
junaem <junh9547@gmail.com>
2022-03-22 17:38:25 +0900

    feat:[back] 모델 수정

--- backend/cities/models.py
+++ backend/cities/models.py
@@ -9,26 +9,32 @@ class Province(models.Model):
     '''
     도(행정구역)
     '''
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
 
 class Cities(models.Model):
     '''
     관광도시 정보
     특별시, 광역시, 자치시, 시, 군 등
     '''
+    id = models.IntegerField(primary_key=True)
     province = models.ForeignKey(Province, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
-    info = models.TextField
-    population = models.IntegerField
-    area = models.IntegerField
-    photo_url = models.TextField
+    info = models.TextField()
+    population = models.IntegerField()
+    area = models.IntegerField()
+    photo_url = models.TextField()
 
+    def __str__(self):
+        return self.name
 
 class Visit(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     city = models.ForeignKey(Cities, on_delete=models.CASCADE)
-    rate = models.FloatField
+    rate = models.FloatField()
 
 
 class Attraction(models.Model):
@@ -40,8 +46,11 @@ class Attraction(models.Model):
     name = models.CharField(max_length=20)
     address = models.CharField(max_length=30)
     facilities = models.TextField(max_length=100)
-    parking_lot = models.IntegerField
-    info = models.TextField
+    parking_lot = models.IntegerField()
+    info = models.TextField()
     tel = models.CharField(max_length=15)
-    latitude = models.FloatField
-    longitude = models.FloatField
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
