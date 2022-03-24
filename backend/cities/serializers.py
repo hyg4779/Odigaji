475492e2341ab70fe82cd4bd33cf31afa26d3cb5
hong <hyg4779@naver.com>
2022-03-24 15:50:35 +0900

    develop pull

--- backend/cities/serializers.py
+++ backend/cities/serializers.py
@@ -1,9 +1,5 @@
 from rest_framework import serializers
-<<<<<<< HEAD
 from .models import Attraction, City, Visit
-=======
-from models import City, Visit
->>>>>>> c9e92496d59ac5e1f20a4194d8cb428438c48227
 
 class visit_serializer(serializers.ModelSerializer):
     class Meta:
@@ -16,7 +12,6 @@ class visit_serializer(serializers.ModelSerializer):
         }
 
         read_only_fields = ('id')
-<<<<<<< HEAD
 
 
 class city_list_serializer(serializers.ModelSerializer):
@@ -29,5 +24,3 @@ class attraction_list_serializer(serializers.ModelSerializer):
     class Meta:
         model = Attraction
         fields = '__all__'
-=======
->>>>>>> c9e92496d59ac5e1f20a4194d8cb428438c48227
