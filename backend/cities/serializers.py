b960b2383ca1c79e6e4cd57ecc36dcf9f5adc610
junaem <junh9547@gmail.com>
2022-03-25 11:09:37 +0900

    feat:[BE] Visit 모델 변경

--- backend/cities/serializers.py
+++ backend/cities/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
-from models import City, Visit
+from .models import Visit
 
 class visit_serializer(serializers.ModelSerializer):
     class Meta:
         model = Visit
-        fields = {
+        fields = (
             "id",
             "rate",
             "city_id",
             "user_id"
-        }
+        )
 
-        read_only_fields = ('id')
+        read_only_fields = ('id',)
