5a72b75c8aa7029d6b26eca5a831ca4bf4e6b727
jinyong <zhaqh146@naver.com>
2022-03-25 21:39:35 +0900

    refactor:[front]지역 선택 코드 리펙토링

--- frontend/src/components/Surveypage/Selected.js
+++ frontend/src/components/Surveypage/Selected.js
@@ -4,7 +4,13 @@ function Selected({ selected }) {
   return (
     <div>
       {selected.map((ele, idx) => {
-        return <div key={idx}>{ele}</div>;
+        return (
+          <>
+            <div key={idx}>{ele}</div>
+            <input type="range" min="1" max="5"></input>
+            <button>X</button>
+          </>
+        );
       })}
     </div>
   );
--- frontend/src/components/Surveypage/TourList.js
+++ frontend/src/components/Surveypage/TourList.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './TourList.css';
-import Search from './Search';
 import Selected from './Selected';
 
 function TourList() {
@@ -17,81 +16,39 @@ function TourList() {
     '양구',
     '구미',
   ];
-  const [inputWord, setInputWord] = useState('');
   const [selected, setSelected] = useState([]);
-  const [words, setWords] = useState([]);
-  const [tourList, setTourList] = useState(tourData);
 
-  function matchData(event) {
-    const inputData = event.target.value;
-    const inputLength = event.target.value.length;
-
-    setInputWord(inputData);
-    if (inputLength === 0) {
-      // 아무것도 입력하지 않은 상태에서는 빈 배열로 바꿔준다
-      setWords([]);
+  function addTour(event) {
+    event.preventDefault();
+    const inputWord = event.target[0].value;
+    if (inputWord.length === 0) return;
+    if (!tourData.includes(inputWord)) {
+      alert('저희 서비스에는 해당 지역이 없습니다');
+    } else if (selected.includes(inputWord)) {
+      alert('이미 한 번 선택하신 지역입니다.');
     } else {
-      // 사용자가 입력한 단어로 시작하는 지역을 가진 배열로 바꿔준다
-      const wordDatas = tourList.filter(
-        (target) => target.substring(0, inputLength) === inputData
-      );
-      setWords(wordDatas);
-    }
-  }
-
-  function enterKey() {
-    if (window.event.keyCode === 13) {
-      if (!tourData.includes(inputWord)) {
-        alert('저희 서비스에는 해당 지역이 없습니다');
-      } else if (selected.includes(inputWord)) {
-        alert('이미 한 번 선택하신 지역입니다.');
-      } else {
-        setSelected(selected.concat(inputWord));
-        setWords([]);
-        setInputWord('');
-        setTourList(tourList.filter((data) => data !== inputWord));
-      }
-    }
-  }
-
-  function addKey() {
-    if (inputWord.length >= 1) {
       setSelected(selected.concat(inputWord));
-      setWords([]);
-      setInputWord('');
-      setTourList(tourList.filter((data) => data !== inputWord));
+      event.target[0].value = '';
     }
   }
 
-  function selectOption(event) {
-    setInputWord(event.target.value);
+  function makeList() {
+    return tourData.map((data, idx) => {
+      return (
+        <option key={idx} disabled={selected.includes(data)}>
+          {data}
+        </option>
+      );
+    });
   }
 
   return (
     <div className="Tourlist">
-      <div className="Tourlist-input">
-        <input
-          type="search"
-          value={inputWord}
-          onChange={matchData}
-          onKeyUp={enterKey}
-          style={{ width: '100px' }}
-        />
-        {words.length > 0 && (
-          <Search
-            words={words}
-            setInputWord={setInputWord}
-            setWords={setWords}
-          />
-        )}
-        <button onClick={addKey}>추가하기</button>
-      </div>
-      <select onChange={selectOption}>
-        <option value="">지역을 선택하세요</option>
-        {tourList.map((data, idx) => {
-          return <option key={idx}>{data}</option>;
-        })}
-      </select>
+      <form onSubmit={(event) => addTour(event)}>
+        <input list="tourInput" style={{ width: '100px' }} />
+        <datalist id="tourInput">{makeList()}</datalist>
+        <button>추가하기</button>
+      </form>
       <Selected selected={selected} />
     </div>
   );
