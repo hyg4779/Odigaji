075fe0bc6f4386f0ab746349051cedd8db5064d5
김석원 <seokworking1609@gmail.com>
2022-03-25 11:24:37 +0900

    design:[front]지역페이지 디자인수정

--- frontend/src/pages/Travelpage/Local.js
+++ frontend/src/pages/Travelpage/Local.js
@@ -1,23 +1,40 @@
 import { Container, Image, Row, Table, Col, Button } from 'react-bootstrap';
 import React from 'react';
 function Local() {
+  let citydata = [
+    '수원 관광지 목록',
+    '인구수 : 4만',
+    '면적 : 23000M^2',
+    '/img/수원시.jpg',
+    '도시소개 : 수원시(水原市)는 대한민국 경기도 서남부에 있는 \
+  특례시이자 경기도청 소재지이다. 동쪽으로는 용인시, 서쪽으로는\
+  안산시, 남쪽으로는 화성시, 북쪽으로 의왕시와 접한다. 시청 소재지는\
+  팔달구 인계동이며, 장안구, 권선구, 팔달구, 영통구의 4개 일반구가\
+  설치되어 있다.',
+  ];
   let tempdata = ['수원화성', '수원화성2', '수원화성3'];
   return (
-    <div className="TravelDetail">
+    <div className="TravelDetail m-5 pt-5">
       <div className="list">
-        <Row>
-          <Col sm={6}>수원 관광지 목록</Col>
-          <Col sm={3}>인구수 : 4만</Col>
-          <Col sm={3}>면적 : 23000M^2</Col>
+        <Row className="h3">
+          <Col sm={4} className="bg-secondary text-white m-2">
+            수원 관광지 목록
+          </Col>
+          <Col sm={2} className="bg-secondary text-white m-2">
+            인구수 : 4만
+          </Col>
+          <Col sm={4} className="bg-secondary text-white m-2">
+            면적 : 23000M^2
+          </Col>
         </Row>
       </div>
       <div className="listContents">
         <Row>
           <Col>
-            <Image src="/img/수원시.jpg" roundedCircle />
+            <Image src="/img/수원시.jpg" rounded />
           </Col>
           <Col>
-            <div className="cityintro">
+            <div className="cityintro bg-secondary text-white m-2">
               도시소개 : 수원시(水原市)는 대한민국 경기도 서남부에 있는
               특례시이자 경기도청 소재지이다. 동쪽으로는 용인시, 서쪽으로는
               안산시, 남쪽으로는 화성시, 북쪽으로 의왕시와 접한다. 시청 소재지는
@@ -28,7 +45,7 @@ function Local() {
         </Row>
       </div>
       {/* 부트스트랩 테이블 넣을자리!!! */}
-      <Table striped bordered hover>
+      <Table striped bordered hover className="m-5">
         <tbody>
           {/* {props.tempdata.map((data,index)) => {
             (<tr key={index}>
@@ -39,7 +56,7 @@ function Local() {
           {tempdata.map((data, idx) => {
             return (
               <tr key={idx}>
-                <td>{data}</td>
+                <td className="m-2">{data}</td>
               </tr>
             );
           })}
