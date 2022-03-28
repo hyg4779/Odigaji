fadbe5fba119ebb899e72bf1675520995ef8a992
junaem <junh9547@gmail.com>
2022-03-28 17:23:14 +0900

    feat:[BE] recommends 스웨거 추가

--- backend/recommends/urls.py
+++ backend/recommends/urls.py
@@ -6,7 +6,8 @@ from . import views
 app_name = 'recommends'
 urlpatterns = [
     path('sel-city/', views.sel_city, name='sel_city'),
-    path('taste/', views.sel_taste, name='sel_taste'),
+    path('knn/', views.by_big_data, name='by_random'),
+    path('taste/', views.taste, name='sel_taste'),
     path('popular/<int:n>', views.popular, name='popular'),
     path('random/', views.by_random, name='by_random'),
 ]
