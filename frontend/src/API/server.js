52fcd92e30c29fe879e1d762eca3020cee4bdc24
jinyong <zhaqh146@naver.com>
2022-03-30 17:39:52 +0900

    feat:[front]server.js 수정

--- frontend/src/API/server.js
+++ frontend/src/API/server.js
@@ -9,11 +9,8 @@ const server = {
     password: '/api/accounts/mypage/password/',
     allCities: '/api/cities/',
     attraction: '/get-attraction/',
-<<<<<<< HEAD
     popular: '/api/recommends/popular/',
-=======
     review: '/api/reviews/',
->>>>>>> 69d330133ae4c2ea9b5cc5ccc69d8214a7e1f7e0
   },
 };
 
