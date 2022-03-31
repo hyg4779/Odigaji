b02e21610de37b2e463f13105bfc96cd05300a29
junaem <junh9547@gmail.com>
2022-03-31 16:38:14 +0900

    feat:[BE] Taste 프론트 연동

--- backend/Sub_PJT3/settings.py
+++ backend/Sub_PJT3/settings.py
@@ -176,12 +176,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ORIGIN_ALLOW_ALL=True
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_CREDENTIALS = True
-# CORS_ORIGIN_WHITELIST = [
-#        "http://localhost:8000",
-#        "http://localhost:3000",
-#        "http://127.0.0.1:8000",
-#        "http://127.0.0.1:3000",
-# ]
+CORS_ORIGIN_WHITELIST = [
+       "http://localhost:8000",
+       "http://localhost:3000",
+       "http://127.0.0.1:8000",
+       "http://127.0.0.1:3000",
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
