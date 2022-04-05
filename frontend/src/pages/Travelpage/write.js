ebde995cc1982e6a2b5b47f43e90eea93d9caf38
안재현 <sevct@naver.com>
2022-04-05 17:46:33 +0900

    fix:[FE]html 렌더링 수정

--- frontend/src/pages/Travelpage/write.js
+++ frontend/src/pages/Travelpage/write.js
@@ -72,7 +72,7 @@ function Write() {
         />
         <CKEditor
           editor={ClassicEditor}
-          data="<p>Hello from CKEditor 5!</p>"
+          data=""
           onReady={(editor) => {
             // You can store the "editor" and use when it is needed.
             console.log('Editor is ready to use!', editor);
