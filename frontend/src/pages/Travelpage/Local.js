5898c65a9d6a23ab34d482f93854b3fcf6837042
윤숙 <dbstnr2146@naver.com>
2022-04-06 10:43:46 +0900

    Design : [front] 카카오 지도연동

--- frontend/src/pages/Travelpage/Local.js
+++ frontend/src/pages/Travelpage/Local.js
@@ -1,3 +1,4 @@
+/*global kakao*/
 import { Table } from 'react-bootstrap';
 
 import React, { useState, useEffect } from 'react';
@@ -15,26 +16,6 @@ function MoveBoardPage(id) {
   window.location.href = 'travelDetail/board/' + id + '/';
 }
 
-// function visited() {
-//   console.log('visited button clicked');
-//   const jwt = sessionStorage.getItem('jwt');
-//   axios.defaults.headers.common['Authorization'] = jwt ? `Bearer ${jwt}` : '';
-
-//   axios
-//     .post(server.BASE_URL + server.ROUTES.selCity, {
-//       city: cityId,
-//       rate: 5,
-//     })
-//     .then((res) => {
-//       console.log(res);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-
-//   // 나중에 방문처리 할것!!!
-//   // axios.get();
-// }
 let cityId;
 function Local() {
   let params = useParams();
@@ -47,15 +28,18 @@ function Local() {
   const [area, setArea] = useState(null); // 시 넓이
   const [travelList, setTravelList] = useState(null); // 시 내 관광지목록
   const [localLogo, setLocalLogo] = useState(null); // 시 로고 이미지
+  const [localImg, setLocalImg] = useState(null); // 시 로고 이미지
   const [avg_rate, setAvg_rate] = useState(null);
   const [rating, setRating] = useState(null);
   const [modalOpen, setModalOpen] = useState(false);
+
   const openModal = () => {
     setModalOpen(true);
   };
   const closeModal = () => {
     setModalOpen(false);
   };
+
   //////////////////// URL //////////////////////////////////////
   const cityUrl =
     server.BASE_URL + server.ROUTES.allCities + cityId + '/' + 'get-city/';
@@ -63,14 +47,41 @@ function Local() {
     server.BASE_URL + server.ROUTES.allCities + cityId + '/is-visited/';
   let isLogin = sessionStorage.getItem('jwt');
 
+  const randerMap = (att_ListData) => {
+    console.log(att_ListData);
+    var mapContainer2 = document.getElementById('map2'), // 지도를 표시할 div
+      mapOption2 = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+      };
+    var map2 = new kakao.maps.Map(mapContainer2, mapOption2); // 지도를 생성합니다
+    // 마커가 표시될 위치입니다
+    att_ListData &&
+      att_ListData.forEach((data, key) => {
+        console.log(data);
+        var markerPosition = new kakao.maps.LatLng(
+          data.latitude,
+          data.longitude
+        );
+        var marker = new kakao.maps.Marker({
+          position: markerPosition,
+        });
+        marker.setMap(map2);
+      });
+    // var markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+    // 마커를 생성합니다
+    // var marker = new kakao.maps.Marker({
+    //   position: markerPosition,
+    // });
+    // 마커가 지도 위에 표시되도록 설정합니다
+  };
   useEffect(() => {
     isLogin = sessionStorage.getItem('jwt');
-
+    let att_ListData = [];
     const jwt = sessionStorage.getItem('jwt');
     axios.defaults.headers.common['Authorization'] = jwt ? `Bearer ${jwt}` : '';
     //도시정보 가져오기!!!
     axios.get(cityUrl).then((res) => {
-      console.log(res);
       setId(res.data.id);
       setInfo(res.data.info);
       setPapulation(res.data.population);
@@ -78,9 +89,17 @@ function Local() {
       setProvince(res.data.province_data.name);
       setArea(res.data.area);
       setTravelList(res.data.att_data);
-      setLocalLogo(res.data.background_photo);
+
+      res.data.att_data.map((data) => {
+        att_ListData.push({
+          latitude: data.latitude,
+          longitude: data.longitude,
+        });
+        // console.log(data.latitude, data.longitude);
+      });
+      setLocalLogo(res.data.photo);
+      setLocalImg(res.data.background_photo);
       setAvg_rate(res.data.avg_rate);
-      console.log(server.BASE_URL + localLogo);
     });
     //내 별점 가져오기
     axios
@@ -94,6 +113,7 @@ function Local() {
       .catch((err) => {
         isLogin = false;
       });
+    randerMap(att_ListData);
   }, [isLogin]);
 
   return (
@@ -101,7 +121,7 @@ function Local() {
       <div className="DetailContent">
         <div className="LocalInfoBox">
           <div className="LocalWrap">
-            <img className="localImage" src={server.BASE_URL + localLogo} />
+            <img className="localImage" src={server.BASE_URL + localImg} />
             <div className="LocalInfo">
               <div className="LocalTitle">
                 {(province === '자치 시도' ? '' : province) + '\n' + name}
@@ -110,12 +130,16 @@ function Local() {
                 ) : (
                   ''
                 )}
+                <img
+                  className="LocalLogoImg"
+                  src={server.BASE_URL + localLogo}
+                ></img>
               </div>
               <div className="spaceArea"></div>
               <div className="LcoalText">{info}</div>
               <div className="spaceArea"></div>
-              <div>인구수 : {papulation}</div>
-              <div>면적 : {area}</div>
+              <div>인구수 : 약 {papulation} 명 </div>
+              <div>면적 : {area} m2</div>
             </div>
           </div>
 
@@ -128,19 +152,23 @@ function Local() {
                 setRating={setRating}
               />
             </div>
-            {(isLogin && rating) != null ? (
-              <div className="RatingTitle2">
-                <span className="avg">나의 평점</span>
-                <TravelRating
-                  cityId={cityId}
-                  rating={rating}
-                  setRating={setRating}
-                />
-              </div>
+            {isLogin != null ? (
+              rating != null ? (
+                <div className="RatingTitle2">
+                  <span className="avg">나의 평점</span>
+                  <TravelRating
+                    cityId={cityId}
+                    rating={rating}
+                    setRating={setRating}
+                  />
+                </div>
+              ) : (
+                <button className="MyRatingTitle2" onClick={openModal}>
+                  다녀온 곳 등록
+                </button>
+              )
             ) : (
-              <button className="MyRatingTitle2" onClick={openModal}>
-                다녀온 곳 등록
-              </button>
+              ''
             )}
           </div>
         </div>
@@ -163,6 +191,7 @@ function Local() {
               리뷰 게시판
             </button>
           </div>
+          <div id="map2" style={{ width: '100%', height: '350px' }}></div>
           <Table striped bordered hover>
             <tbody>
               {/* &&양옆에 2개쓰면  */}
@@ -183,72 +212,6 @@ function Local() {
         <div className="modal_body">Modal</div>
       </div>
     </div>
-
-    // <div className="TravelDetail m-5 pt-5 container">
-    //   <div className="card col-sm-4 mt-5">
-    //     <img
-    //       className="card-img-top"
-    //       src={server.BASE_URL + localLogo}
-    //       alt="Card image"
-    //     />
-    //     <div className="card-body">
-    //       <h2 className="card-title">{name}</h2>
-    //       <h5 className="card-text">{info}</h5>
-    //       <div className="bg-info">
-    //         평균별점
-    //         <TravelRating
-    //           cityId={cityId}
-    //           rating={avg_rate}
-    //           setRating={setRating}
-    //         />
-    //       </div>
-    //       <div>인구수 : {papulation}</div>
-    //       <div>면적 : {area}</div>
-    //       <div className="bg-info">
-    //         {isLogin != null ? (
-    //           <TravelRating
-    //             cityId={cityId}
-    //             rating={rating}
-    //             setRating={setRating}
-    //           />
-    //         ) : null}
-    //       </div>
-    //       <Button
-    //         className="reviewButton"
-    //         variant="primary"
-    //         onClick={() => MoveBoardPage(params.cityId)}
-    //       >
-    //         리뷰 게시판 이동
-    //       </Button>{' '}
-    //     </div>
-    //     {/* end card */}
-    //   </div>
-    //   <div className="col-sm-6 tablediv mt-5 pt-5">
-    //     <Table striped bordered hover>
-    //       <thead className="bg-info">
-    //         <th>
-    //           <h5>{name}여행지목록</h5>
-    //         </th>
-    //       </thead>
-    //       <tbody>
-    //         {/* &&양옆에 2개쓰면  */}
-    //         {travelList &&
-    //           travelList.map((data, idx) => {
-    //             return (
-    //               //무명함수 호출안하면 강제로 이동한다....
-    //               <tr key={idx} onClick={() => MovetravelPage(data.id)}>
-    //                 <td className="m-2">{data.name}</td>
-    //               </tr>
-    //             );
-    //           })}
-    //       </tbody>
-    //     </Table>
-    //   </div>
-
-    //   {/* 로그인여부에따라 별점 평가 기능 활성/ 비활성화 */}
-    // </div>
-
-    // end TravelDetail Div
   );
 }
 
